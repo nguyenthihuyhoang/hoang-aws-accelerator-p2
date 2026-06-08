d750636b211f15966b2154823b134e71e5ad98cc
nguyenthihuyhoang <nguyenthihuyhoang1104@gmail.com>
2026-06-08 22:12:30 +0700

    Implement Terraform Final Project structure and code

--- W8/lab/terraform-Final-Project/backend.tf
+++ W8/lab/terraform-Final-Project/backend.tf
@@ -1,11 +1,19 @@
-# Terraform Backend Configuration
-# Configures S3 Backend & DynamoDB for State Locking
 terraform {
-  # backend "s3" {
-  #   bucket         = "your-terraform-state-bucket"
-  #   key            = "final-project/terraform.tfstate"
-  #   region         = "us-east-1"
-  #   dynamodb_table = "your-terraform-state-lock"
-  #   encrypt        = true
-  # }
+  backend "s3" {
+    bucket         = "tfstate-newbie" # TODO: Thay bằng bucket S3 lưu state thực tế của bạn
+    key            = "final-project/terraform.tfstate"
+    region         = "ap-southeast-1"
+#    dynamodb_table = "terraform-state-lock"       # Bảng DynamoDB để khóa state
+    encrypt        = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
 }
--- W8/lab/terraform-Final-Project/modules/vpc/variables.tf
+++ W8/lab/terraform-Final-Project/modules/vpc/variables.tf
@@ -1,2 +1,25 @@
-# VPC Module Input Variables
-# e.g., vpc_cidr, public_subnet_cidrs, private_subnet_cidrs, tags
+variable "vpc_cidr" {
+  type        = string
+  description = "The CIDR block for the VPC"
+}
+
+variable "public_cidr" {
+  type        = string
+  description = "The CIDR block for the public subnet"
+}
+
+variable "private_cidr" {
+  type        = string
+  description = "The CIDR block for the private subnet"
+}
+
+variable "region" {
+  type        = string
+  description = "The AWS region"
+  default     = "ap-southeast-1"
+}
+
+variable "env" {
+  type        = string
+  description = "The environment name (e.g. prod, dev)"
+}
